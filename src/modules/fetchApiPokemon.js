d1c816a5b96d4efeb104417418ee4c3460387b5a
Vitor Guedes <vgm_rox@hotmail.com>
2022-07-28 22:50:46 -0300

    set pokemons to 151

--- src/modules/fetchApiPokemon.js
+++ src/modules/fetchApiPokemon.js
@@ -1,5 +1,5 @@
 const fetchApi = async () => {
-  const getPokemons = await fetch('https://pokeapi.co/api/v2/pokemon?limit=3&offset=0');
+  const getPokemons = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0');
   return getPokemons.json();
 };
 
